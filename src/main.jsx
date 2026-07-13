948ed8d07ac170a8bb99336f0fd51a0c6ce1615a
itscsart <contato.camilapaivasouza@gmail.com>
2026-07-13 16:01:27 -0300

    Melhora atualizacao automatica do PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
+
 import './index.css'
 import App from './App.jsx'
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    window.location.reload()
+  },
+  onOfflineReady() {
+    console.log('TrajetEla disponível offline')
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
